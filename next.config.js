41c7d48def9f78a3c0b9753a649ddbb9c08d64a1
lunamoon-00 <b8szsuut4n@yahoo.co.jp>
2026-02-21 16:46:54 +0900

    エラー改善お願いします

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    // .env.local の編集で開発サーバーが再起動しないようにする
+    if (dev && config.watchOptions) {
+      config.watchOptions.ignored = [
+        ...(Array.isArray(config.watchOptions.ignored) ? config.watchOptions.ignored : []),
+        "**/.env.local",
+        "**/.env*.local",
+      ];
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig;
